a22782d5bfdfff94b2f6e3a9937ea51f66dcd73c
yes7843 <50169298+the753q@users.noreply.github.com>
2024-03-28 14:31:29 +0100

    minor fixes

--- PotatoKMeans/Form1.Designer.cs
+++ PotatoKMeans/Form1.Designer.cs
@@ -67,10 +67,9 @@
             // stepBtn
             // 
             stepBtn.FlatStyle = FlatStyle.System;
-            stepBtn.Location = new Point(4, 64);
-            stepBtn.Margin = new Padding(4, 4, 4, 4);
+            stepBtn.Location = new Point(3, 43);
             stepBtn.Name = "stepBtn";
-            stepBtn.Size = new Size(135, 60);
+            stepBtn.Size = new Size(90, 40);
             stepBtn.TabIndex = 1;
             stepBtn.Text = "Step";
             stepBtn.UseVisualStyleBackColor = false;
@@ -80,47 +79,42 @@
             // 
             tableLayoutPanel1.ColumnCount = 3;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100.000008F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 312F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 56F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 208F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 37F));
             tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 0);
             tableLayoutPanel1.Controls.Add(tableLayoutPanel4, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(4, 4);
-            tableLayoutPanel1.Margin = new Padding(4, 4, 4, 4);
+            tableLayoutPanel1.Location = new Point(3, 3);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Size = new Size(966, 597);
+            tableLayoutPanel1.Size = new Size(641, 398);
             tableLayoutPanel1.TabIndex = 2;
             // 
             // tableLayoutPanel2
             // 
             tableLayoutPanel2.ColumnCount = 1;
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
             tableLayoutPanel2.Controls.Add(clusterGroupBox, 0, 1);
             tableLayoutPanel2.Controls.Add(groupBox3, 0, 0);
             tableLayoutPanel2.Dock = DockStyle.Fill;
-            tableLayoutPanel2.Location = new Point(602, 4);
-            tableLayoutPanel2.Margin = new Padding(4, 4, 4, 4);
+            tableLayoutPanel2.Location = new Point(399, 3);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 2;
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 45.8866539F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 54.1133461F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
-            tableLayoutPanel2.Size = new Size(304, 589);
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 183F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 39F));
+            tableLayoutPanel2.Size = new Size(202, 392);
             tableLayoutPanel2.TabIndex = 1;
             // 
             // clusterGroupBox
             // 
             clusterGroupBox.Controls.Add(tableLayoutPanel3);
-            clusterGroupBox.Dock = DockStyle.Fill;
+            clusterGroupBox.Dock = DockStyle.Top;
             clusterGroupBox.Enabled = false;
-            clusterGroupBox.Location = new Point(4, 274);
-            clusterGroupBox.Margin = new Padding(4, 4, 4, 4);
-            clusterGroupBox.MaximumSize = new Size(318, 0);
+            clusterGroupBox.Location = new Point(3, 186);
+            clusterGroupBox.MaximumSize = new Size(212, 0);
             clusterGroupBox.Name = "clusterGroupBox";
-            clusterGroupBox.Padding = new Padding(4, 4, 4, 4);
-            clusterGroupBox.Size = new Size(296, 311);
+            clusterGroupBox.Size = new Size(196, 202);
             clusterGroupBox.TabIndex = 4;
             clusterGroupBox.TabStop = false;
             clusterGroupBox.Text = "Cluster Control";
@@ -136,24 +130,22 @@
             tableLayoutPanel3.Controls.Add(FinishBtn, 0, 2);
             tableLayoutPanel3.Controls.Add(resetBtn, 0, 3);
             tableLayoutPanel3.Dock = DockStyle.Top;
-            tableLayoutPanel3.Location = new Point(4, 28);
-            tableLayoutPanel3.Margin = new Padding(4, 4, 4, 4);
+            tableLayoutPanel3.Location = new Point(3, 19);
             tableLayoutPanel3.Name = "tableLayoutPanel3";
             tableLayoutPanel3.RowCount = 4;
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 69F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 69F));
-            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
-            tableLayoutPanel3.Size = new Size(288, 270);
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 46F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 46F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Absolute, 19F));
+            tableLayoutPanel3.Size = new Size(190, 180);
             tableLayoutPanel3.TabIndex = 3;
             // 
             // label1
             // 
             label1.Dock = DockStyle.Fill;
-            label1.Location = new Point(4, 0);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
-            label1.Size = new Size(175, 60);
+            label1.Size = new Size(114, 40);
             label1.TabIndex = 2;
             label1.Text = "Number of clusters:";
             label1.TextAlign = ContentAlignment.MiddleLeft;
@@ -161,21 +153,19 @@
             // clustersNo
             // 
             clustersNo.Anchor = AnchorStyles.Right;
-            clustersNo.Location = new Point(191, 14);
-            clustersNo.Margin = new Padding(4, 4, 4, 4);
+            clustersNo.Location = new Point(125, 8);
             clustersNo.Minimum = new decimal(new int[] { 2, 0, 0, 0 });
             clustersNo.Name = "clustersNo";
-            clustersNo.Size = new Size(93, 31);
+            clustersNo.Size = new Size(62, 23);
             clustersNo.TabIndex = 2;
             clustersNo.Value = new decimal(new int[] { 2, 0, 0, 0 });
             // 
             // FinishBtn
             // 
             FinishBtn.FlatStyle = FlatStyle.System;
-            FinishBtn.Location = new Point(4, 133);
-            FinishBtn.Margin = new Padding(4, 4, 4, 4);
+            FinishBtn.Location = new Point(3, 89);
             FinishBtn.Name = "FinishBtn";
-            FinishBtn.Size = new Size(135, 60);
+            FinishBtn.Size = new Size(90, 40);
             FinishBtn.TabIndex = 3;
             FinishBtn.Text = "Finish";
             FinishBtn.UseVisualStyleBackColor = false;
@@ -184,10 +174,9 @@
             // resetBtn
             // 
             resetBtn.FlatStyle = FlatStyle.System;
-            resetBtn.Location = new Point(4, 202);
-            resetBtn.Margin = new Padding(4, 4, 4, 4);
+            resetBtn.Location = new Point(3, 135);
             resetBtn.Name = "resetBtn";
-            resetBtn.Size = new Size(135, 60);
+            resetBtn.Size = new Size(90, 40);
             resetBtn.TabIndex = 4;
             resetBtn.Text = "Reset";
             resetBtn.UseVisualStyleBackColor = false;
@@ -196,12 +185,11 @@
             // groupBox3
             // 
             groupBox3.Controls.Add(tableLayoutPanel9);
-            groupBox3.Dock = DockStyle.Fill;
-            groupBox3.Location = new Point(4, 45);
-            groupBox3.Margin = new Padding(4, 45, 4, 4);
+            groupBox3.Dock = DockStyle.Top;
+            groupBox3.Location = new Point(3, 30);
+            groupBox3.Margin = new Padding(3, 30, 3, 3);
             groupBox3.Name = "groupBox3";
-            groupBox3.Padding = new Padding(4, 4, 4, 4);
-            groupBox3.Size = new Size(296, 221);
+            groupBox3.Size = new Size(196, 150);
             groupBox3.TabIndex = 5;
             groupBox3.TabStop = false;
             groupBox3.Text = "Data Control";
@@ -213,13 +201,12 @@
             tableLayoutPanel9.Controls.Add(fileLabel, 0, 0);
             tableLayoutPanel9.Controls.Add(tableLayoutPanel8, 0, 1);
             tableLayoutPanel9.Dock = DockStyle.Fill;
-            tableLayoutPanel9.Location = new Point(4, 28);
-            tableLayoutPanel9.Margin = new Padding(4, 4, 4, 4);
+            tableLayoutPanel9.Location = new Point(3, 19);
             tableLayoutPanel9.Name = "tableLayoutPanel9";
             tableLayoutPanel9.RowCount = 2;
-            tableLayoutPanel9.RowStyles.Add(new RowStyle(SizeType.Absolute, 44F));
+            tableLayoutPanel9.RowStyles.Add(new RowStyle(SizeType.Absolute, 29F));
             tableLayoutPanel9.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel9.Size = new Size(288, 189);
+            tableLayoutPanel9.Size = new Size(190, 128);
             tableLayoutPanel9.TabIndex = 1;
             // 
             // fileLabel
@@ -228,11 +215,11 @@
             fileLabel.BackColor = Color.White;
             fileLabel.BorderStyle = BorderStyle.FixedSingle;
             fileLabel.Dock = DockStyle.Fill;
-            fileLabel.Location = new Point(6, 8);
-            fileLabel.Margin = new Padding(6, 8, 6, 8);
-            fileLabel.MaximumSize = new Size(449, 0);
+            fileLabel.Location = new Point(4, 5);
+            fileLabel.Margin = new Padding(4, 5, 4, 5);
+            fileLabel.MaximumSize = new Size(300, 400);
             fileLabel.Name = "fileLabel";
-            fileLabel.Size = new Size(276, 28);
+            fileLabel.Size = new Size(182, 19);
             fileLabel.TabIndex = 1;
             fileLabel.Text = "Select text file with data";
             fileLabel.TextAlign = ContentAlignment.MiddleLeft;
@@ -245,24 +232,21 @@
             tableLayoutPanel8.Controls.Add(loadFileBtn, 0, 0);
             tableLayoutPanel8.Controls.Add(selectDataBtn, 0, 1);
             tableLayoutPanel8.Dock = DockStyle.Fill;
-            tableLayoutPanel8.Location = new Point(0, 44);
+            tableLayoutPanel8.Location = new Point(0, 29);
             tableLayoutPanel8.Margin = new Padding(0);
             tableLayoutPanel8.Name = "tableLayoutPanel8";
-            tableLayoutPanel8.RowCount = 4;
-            tableLayoutPanel8.RowStyles.Add(new RowStyle(SizeType.Absolute, 70F));
-            tableLayoutPanel8.RowStyles.Add(new RowStyle(SizeType.Absolute, 69F));
-            tableLayoutPanel8.RowStyles.Add(new RowStyle(SizeType.Absolute, 63F));
-            tableLayoutPanel8.RowStyles.Add(new RowStyle(SizeType.Absolute, 18F));
-            tableLayoutPanel8.Size = new Size(288, 145);
+            tableLayoutPanel8.RowCount = 2;
+            tableLayoutPanel8.RowStyles.Add(new RowStyle(SizeType.Absolute, 47F));
+            tableLayoutPanel8.RowStyles.Add(new RowStyle(SizeType.Absolute, 46F));
+            tableLayoutPanel8.Size = new Size(190, 99);
             tableLayoutPanel8.TabIndex = 0;
             // 
             // loadFileBtn
             // 
             loadFileBtn.FlatStyle = FlatStyle.System;
-            loadFileBtn.Location = new Point(4, 4);
-            loadFileBtn.Margin = new Padding(4, 4, 4, 4);
+            loadFileBtn.Location = new Point(3, 3);
             loadFileBtn.Name = "loadFileBtn";
-            loadFileBtn.Size = new Size(135, 60);
+            loadFileBtn.Size = new Size(90, 40);
             loadFileBtn.TabIndex = 0;
             loadFileBtn.Text = "Load File";
             loadFileBtn.UseVisualStyleBackColor = true;
@@ -272,10 +256,9 @@
             // 
             selectDataBtn.Enabled = false;
             selectDataBtn.FlatStyle = FlatStyle.System;
-            selectDataBtn.Location = new Point(4, 74);
-            selectDataBtn.Margin = new Padding(4, 4, 4, 4);
+            selectDataBtn.Location = new Point(3, 50);
             selectDataBtn.Name = "selectDataBtn";
-            selectDataBtn.Size = new Size(135, 60);
+            selectDataBtn.Size = new Size(90, 40);
             selectDataBtn.TabIndex = 1;
             selectDataBtn.Text = "Select Data";
             selectDataBtn.UseVisualStyleBackColor = true;
@@ -288,37 +271,36 @@
             tableLayoutPanel4.Controls.Add(tableLayoutPanel11, 0, 1);
             tableLayoutPanel4.Controls.Add(formsPlot1, 0, 0);
             tableLayoutPanel4.Dock = DockStyle.Fill;
-            tableLayoutPanel4.Location = new Point(4, 4);
-            tableLayoutPanel4.Margin = new Padding(4, 4, 4, 4);
+            tableLayoutPanel4.Location = new Point(3, 3);
             tableLayoutPanel4.Name = "tableLayoutPanel4";
             tableLayoutPanel4.RowCount = 2;
             tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Absolute, 45F));
-            tableLayoutPanel4.Size = new Size(590, 589);
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel4.Size = new Size(390, 392);
             tableLayoutPanel4.TabIndex = 2;
             // 
             // tableLayoutPanel11
             // 
             tableLayoutPanel11.ColumnCount = 2;
-            tableLayoutPanel11.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 182F));
-            tableLayoutPanel11.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 406F));
+            tableLayoutPanel11.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 121F));
+            tableLayoutPanel11.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 272F));
             tableLayoutPanel11.Controls.Add(AutoScaleBtn, 0, 0);
             tableLayoutPanel11.Dock = DockStyle.Fill;
-            tableLayoutPanel11.Location = new Point(0, 544);
+            tableLayoutPanel11.Location = new Point(0, 362);
             tableLayoutPanel11.Margin = new Padding(0);
             tableLayoutPanel11.Name = "tableLayoutPanel11";
             tableLayoutPanel11.RowCount = 1;
             tableLayoutPanel11.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel11.Size = new Size(590, 45);
+            tableLayoutPanel11.Size = new Size(390, 30);
             tableLayoutPanel11.TabIndex = 5;
             // 
             // AutoScaleBtn
             // 
             AutoScaleBtn.FlatStyle = FlatStyle.System;
-            AutoScaleBtn.Location = new Point(60, 4);
-            AutoScaleBtn.Margin = new Padding(60, 4, 4, 4);
+            AutoScaleBtn.Location = new Point(40, 3);
+            AutoScaleBtn.Margin = new Padding(40, 3, 3, 3);
             AutoScaleBtn.Name = "AutoScaleBtn";
-            AutoScaleBtn.Size = new Size(112, 34);
+            AutoScaleBtn.Size = new Size(75, 23);
             AutoScaleBtn.TabIndex = 1;
             AutoScaleBtn.Text = "Reset Scale";
             AutoScaleBtn.UseVisualStyleBackColor = true;
@@ -328,10 +310,9 @@
             // 
             formsPlot1.DisplayScale = 1F;
             formsPlot1.Dock = DockStyle.Fill;
-            formsPlot1.Location = new Point(4, 4);
-            formsPlot1.Margin = new Padding(4, 4, 4, 4);
+            formsPlot1.Location = new Point(3, 3);
             formsPlot1.Name = "formsPlot1";
-            formsPlot1.Size = new Size(582, 536);
+            formsPlot1.Size = new Size(384, 356);
             formsPlot1.TabIndex = 0;
             // 
             // groupBox2
@@ -379,24 +360,23 @@
             statusStrip1.Dock = DockStyle.Fill;
             statusStrip1.ImageScalingSize = new Size(24, 24);
             statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, StatusLabel });
-            statusStrip1.Location = new Point(0, 605);
+            statusStrip1.Location = new Point(0, 404);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Padding = new Padding(2, 0, 21, 0);
-            statusStrip1.Size = new Size(974, 39);
+            statusStrip1.Size = new Size(647, 26);
             statusStrip1.TabIndex = 3;
             statusStrip1.Text = "statusStrip1";
             // 
             // toolStripStatusLabel1
             // 
             toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            toolStripStatusLabel1.Size = new Size(64, 32);
+            toolStripStatusLabel1.Size = new Size(42, 21);
             toolStripStatusLabel1.Text = "Status:";
             // 
             // StatusLabel
             // 
             StatusLabel.BorderSides = ToolStripStatusLabelBorderSides.Left | ToolStripStatusLabelBorderSides.Top | ToolStripStatusLabelBorderSides.Right | ToolStripStatusLabelBorderSides.Bottom;
             StatusLabel.Name = "StatusLabel";
-            StatusLabel.Size = new Size(887, 32);
+            StatusLabel.Size = new Size(590, 21);
             StatusLabel.Spring = true;
             StatusLabel.Text = "STATUS!!!!!";
             // 
@@ -412,22 +392,20 @@
             tableLayoutPanel10.Controls.Add(tableLayoutPanel1, 0, 0);
             tableLayoutPanel10.Dock = DockStyle.Fill;
             tableLayoutPanel10.Location = new Point(0, 0);
-            tableLayoutPanel10.Margin = new Padding(4, 4, 4, 4);
             tableLayoutPanel10.Name = "tableLayoutPanel10";
             tableLayoutPanel10.RowCount = 2;
             tableLayoutPanel10.RowStyles.Add(new RowStyle(SizeType.Percent, 100.000008F));
-            tableLayoutPanel10.RowStyles.Add(new RowStyle(SizeType.Absolute, 39F));
-            tableLayoutPanel10.Size = new Size(974, 644);
+            tableLayoutPanel10.RowStyles.Add(new RowStyle(SizeType.Absolute, 26F));
+            tableLayoutPanel10.Size = new Size(647, 430);
             tableLayoutPanel10.TabIndex = 4;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(144F, 144F);
+            AutoScaleDimensions = new SizeF(96F, 96F);
             AutoScaleMode = AutoScaleMode.Dpi;
-            ClientSize = new Size(974, 644);
+            ClientSize = new Size(647, 430);
             Controls.Add(tableLayoutPanel10);
-            Margin = new Padding(4, 4, 4, 4);
-            MinimumSize = new Size(986, 674);
+            MinimumSize = new Size(663, 469);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "K-Means Clustering";
